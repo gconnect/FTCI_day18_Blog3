e5b8ae2dc1fa75d9cf2e10430a18aa6ca22f6e3e
David Adams <david.riding.rails@gmail.com>
2020-06-23 20:37:35 -0700

    initial commit

---
+++ src/test/java/com/tts/ttsBlog3/TtsBlog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ttsBlog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsBlog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
