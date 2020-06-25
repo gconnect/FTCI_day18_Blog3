4f105f2a35aad647348d456350bab1c47f4f7a33
David Adams <david.riding.rails@gmail.com>
2020-06-24 18:41:50 -0700

    refactor article repository

--- src/main/java/com/tts/ttsBlog3/repository/ArticleRepository.java
+++ src/main/java/com/tts/ttsBlog3/repository/ArticleRepository.java
@@ -1,12 +1,13 @@
 package com.tts.ttsBlog3.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tts.ttsBlog3.model.Article;
 
 @Repository
-public interface ArticleRepository
-    extends CrudRepository<Article, Long> {
-  public Article findArticleById(Long article_id);
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+  public Optional<Article> findById(Long articleId);
 }
